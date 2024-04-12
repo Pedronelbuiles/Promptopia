e7c1e7808fdaf0bbae941642fbf7d0fbff8ecfcf
Pedro Nel Builes Moreno <pedronel.builes@gmail.com>
2024-04-12 00:04:58 -0500

    adding nsv and components - video in 1:12:05

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
+import { Feed } from '@components/Feed'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="2-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative promopts
+      </p>
+      <Feed />
+    </section>
   )
 }
 
